812682a3d0defbcb5c00cb30604f8a1e104a9d7c
filipi <filipichip7@gmail.com>
2023-04-26 20:16:03 -0300

    finished loop through a dummy database on templates

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'gallery/index.html')
+    gallery_data = {
+    1: {
+        "name": "Carina Nebula",
+        "caption": "webbtelescope.org / NASA / James Webb",
+       },
+    2: {
+       "name": "NGC 1079 Galaxy",
+       "caption": "nasa.org / NASA / Hubble", 
+       },
+    }
+    
+    return render(request, 'gallery/index.html', {
+        "cards": gallery_data
+    })
 
 def image(request):
     return render(request, 'gallery/imagem.html')
