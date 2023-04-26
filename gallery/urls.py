69e4a102326c23d89b840f775b09a96cade789ae
filipi <filipichip7@gmail.com>
2023-04-26 16:53:56 -0300

    configuring gallery/image templates

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from gallery.views import index
+from gallery.views import index, image
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('image/', image),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'gallery/index.html')
+
+def image(request):
+    return render(request, 'gallery/imagem.html')
