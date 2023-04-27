fc214bd83cae29c05c4b1088f48b6d0a71ca041a
filipi <filipichip7@gmail.com>
2023-04-26 21:48:05 -0300

    more admin adjusts

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from gallery.models import Photograph
+
+class PhotosList(admin.ModelAdmin):
+    list_display = ('id', 'name', 'caption')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    
+
 # Register your models here.
+
+admin.site.register(Photograph, PhotosList)
--- gallery/models.py
+++ gallery/models.py
@@ -3,8 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Photograph(models.Model):
+    
+    CATEGORY_OPTIONS = [
+        ("NEBULA", "Nebula"),
+        ("STAR", "Star"),
+        ("GALAXY", "Galaxy"),
+        ("PLANET", "Planet")
+    ]
+    
     name = models.CharField(max_length=100, null=False, blank=False)
     caption = models.CharField(max_length=150, null=False, blank=False)
+    category = models.CharField(max_length=100, choices=CATEGORY_OPTIONS, default='')
     description = models.TextField(null=False, blank=False)
     photo = models.CharField(max_length=100, null=False, blank=False)
     
