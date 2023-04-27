817b204afe7e54957c99f3ca58f295c718c11117
filipi <filipichip7@gmail.com>
2023-04-27 16:08:06 -0300

    structured register form/views/template

--- users/forms.py
+++ users/forms.py
@@ -23,3 +23,49 @@ class LoginForm(forms.Form):
             }
         )
     )
+
+class RegisterForm(forms.Form):
+    register_name = forms.CharField(
+        label="Username",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: John Doe"
+            }
+        )
+    )
+    email = forms.EmailField(
+        label="Email",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: johndoe@example.com"
+            }
+        )
+    )
+    password_1 = forms.CharField(
+        label="Password",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter your password"
+            }
+        )
+    )
+    password_2 = forms.CharField(
+        label="Confirm Password",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Confirm your password"
+            }
+        )
+    )
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from users.forms import LoginForm
+from users.forms import LoginForm, RegisterForm
+
+from django.contrib.auth.models import User
 
 def login(request):
     form = LoginForm()
@@ -9,4 +11,31 @@ def login(request):
     })
 
 def register(request):
-    return render(request, 'users/register.html')
+    form = RegisterForm()
+    
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        
+        if form.is_valid():
+            if form['password_1'].value() != form['password_2'].value():
+                return redirect('register')
+            
+            name = form['register_name'].value()
+            email = form['email'].value()
+            password = form['password_1'].value()
+            
+            if User.objects.filter(username=name).exists():
+                return redirect('register')
+            
+            user = User.objects.create_user(
+                username=name,
+                email=email,
+                password=password
+            )
+            user.save()
+            
+            return redirect('login')
+    
+    return render(request, 'users/register.html', {
+        "form": form
+    })
