d1a98bba2e1942c02519ad4d62fa76a9f543b2d8
filipi <filipichip7@gmail.com>
2023-04-26 22:57:35 -0300

    working on image not found

--- gallery/models.py
+++ gallery/models.py
@@ -17,7 +17,7 @@ class Photograph(models.Model):
     caption = models.CharField(max_length=150, null=False, blank=False)
     category = models.CharField(max_length=100, choices=CATEGORY_OPTIONS, default='')
     description = models.TextField(null=False, blank=False)
-    photo = models.CharField(max_length=100, null=False, blank=False)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
     published = models.BooleanField(default=False)
     date = models.DateTimeField(default=datetime.now, blank=False)
     
--- setup/settings.py
+++ setup/settings.py
@@ -127,6 +127,12 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Media Upload / Files
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIAL_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
